0ad8ad3342b49d5360c5a2d84f25b3a57c9eabce
JikunLiu101 <1015482278@qq.com>
2025-09-16 21:08:52 +0800

    Add ExampleChallenge and Playground

--- src/main/java/com/example/challenges/ExampleChallenge/ExampleChallenge.java
+++ src/main/java/com/example/challenges/ExampleChallenge/ExampleChallenge.java
@@ -1,7 +1,42 @@
 package com.example.challenges.ExampleChallenge;
 
-public class ExampleChallenge {
-    public int add(int a, int b) {
-        return a + b;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExampleChallenge{
+    public static void main(){
+        Food food1 = new Food(1, "Burger", 5.0);
+        Resturant resturant1 = new Resturant(1, 4.5, 2.3, new ArrayList<>());
+
+        System.out.println("Hello World");
     }
+
+    public static class Food {
+        Integer resturantId;
+        String name;
+
+        Double price;
+
+        Food(Integer resturantId, String name, Double price){
+            resturantId = resturantId;
+            name = name;
+            price = price;
+        }
+    }
+
+    public static class Resturant {
+        Integer id;
+        Double positionX;
+        Double positionY;
+        List<Food> foodList;
+
+        Resturant(Integer id, Double positionX, Double positionY, List<Food> foodList){
+            id = id;
+            positionX = positionX;
+            positionY = positionY;
+            foodList = foodList;
+        }
+    }
+
+
 }
--- src/main/java/com/example/challenges/Playground/Playground.java
+++ src/main/java/com/example/challenges/Playground/Playground.java
@@ -1,7 +1,82 @@
 package com.example.challenges.Playground;
 
+import com.sun.javafx.geom.BaseBounds;
+import com.sun.javafx.geom.transform.BaseTransform;
+import com.sun.javafx.jmx.MXNodeAlgorithm;
+import com.sun.javafx.jmx.MXNodeAlgorithmContext;
+import com.sun.javafx.sg.prism.NGNode;
+import javafx.scene.Node;
+
 import java.util.*;
 
 public class Playground {
+    public void backtrace(){
+        /**
+         * if (end condition) {
+         *     save the result
+         *     return;
+         * }
+         *
+         * for (every element in this layer){
+         *     handle the element
+         *     backtrace()
+         *     revert the handling for the element
+         * }
+         */
+        int[] paths = new int[0];
+        Queue<Integer> q = new LinkedList<>();
+        q.remove();
 
+    }
+
+    public static void main(String[] args){
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
+}
+
+
+class MyThread extends Thread{
+    @Override
+    public void run(){
+        System.out.println("Hello " + "world");
+    }
+}
+
+class PrintString{
+    private final String text = "Hello world";
+    private int index = 0;
+    private final Object lock = new Object();
+
+    public static void main(String[] args){
+        PrintString ps = new PrintString();
+        Thread t1 = new Thread(()-> ps.printChars("T1"));
+        Thread t2 = new Thread(()-> ps.printChars("T2"));
+
+        t1.start();
+        t2.start();
+    }
+
+    public void printChars(String name){
+        while (true){
+            synchronized (lock){
+                if (index >= text.length()){
+                    lock.notifyAll(); // wake up others before exiting
+                    break;
+                }
+                char c = text.charAt(index++);
+                System.out.print(name+" prints: "+c +"\n");
+                // wake up another thread to continue
+                lock.notifyAll();
+
+                try {
+                    if (index < text.length()){
+                        lock.wait();
+                    }
+                } catch (InterruptedException e){
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
 }
